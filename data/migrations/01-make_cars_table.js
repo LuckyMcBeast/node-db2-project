8f53b4bb285b157dd905d9a4c392efcbf39d9ed0
mCullen-mcclellan <>
2021-03-30 20:00:03 -0400

    Created Cars Schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,16 @@
 // DO YOUR MAGIC
+exports.up = function (knex) {
+    return knex.schema.createTable('cars', tbl =>{
+        tbl.increments('id');
+        tbl.string('vin', 17).unique().notNullable();
+        tbl.string('make', 128).notNullable();
+        tbl.string('model', 128).notNullable();
+        tbl.decimal('mileage');
+        tbl.string('title', 128).notNullable();
+        tbl.string('transmission', 128).notNullable();
+    })
+}
+
+exports.down = function(knex){
+    return knex.schema.dropTableIfExists('cars');
+}
