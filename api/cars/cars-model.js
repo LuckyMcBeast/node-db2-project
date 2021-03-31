4456e14612e694d644943c4d5ec21b14786d8091
mCullen-mcclellan <>
2021-03-30 20:10:51 -0400

    Created Cars Model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,23 @@
+const db = require("../../data/db-config.js")
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('cars');
 }
 
-const getById = () => {
+const getById = id => {
   // DO YOUR MAGIC
+  return db('cars').where({id});
 }
 
-const create = () => {
+const create = async (car) => {
   // DO YOUR MAGIC
+  const createdCar = await db('cars').insert(car);
+  return {message : `New Car Created.`, car : getById(createdCar.id)}
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create
 }
